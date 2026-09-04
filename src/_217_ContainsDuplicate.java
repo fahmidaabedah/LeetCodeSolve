c0f446e210766a300f6da088867d2f653d6ce466
Fahmida Yesmin Abedah <fahmida241-35-396@diu.edu.bd>
2026-09-04 19:11:25 +0600

    Solve

---
+++ src/_217_ContainsDuplicate.java
@@ -0,0 +1,15 @@
+public class _217_ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        boolean result = false;
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    result = true;
+                    return result;
+                }
+            }
+        }
+
+        return result;
+    }
+}
