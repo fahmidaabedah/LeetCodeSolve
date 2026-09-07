209ef77c3f8314b483018bd9051023cbecfb0aa7
Fahmida Yesmin Abedah <fahmida241-35-396@diu.edu.bd>
2026-09-07 20:25:44 +0600

    Solve

---
+++ src/_238_ProductofArrayExceptSelf.java
@@ -0,0 +1,18 @@
+public class _238_ProductofArrayExceptSelf {
+    public int[] productExceptSelf(int[] nums) {
+        int[] answer = new int[nums.length];
+        //left prod
+        int left = 1;
+        for (int i = 0; i < nums.length; i++) {
+            answer[i] = left;
+            left = nums[i] * left;
+        }
+        //right prod(final)
+        int right = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            answer[i] = right * answer[i];
+            right = right * nums[i];
+        }
+        return answer;
+    }
+}
