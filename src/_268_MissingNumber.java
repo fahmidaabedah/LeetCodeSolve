c07a2e361718323396e1ca69539929151eeffed9
Fahmida Yesmin Abedah <fahmida241-35-396@diu.edu.bd>
2026-09-10 14:14:56 +0600

    Solve

---
+++ src/_268_MissingNumber.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+public class _268_MissingNumber {
+    public int missingNumber(int[] nums) {
+        int actual_sum = 0;
+        int nums_sum = 0;
+        for (int i = 0; i <= nums.length; i++) {
+            actual_sum += i;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            nums_sum += nums[i];
+        }
+        return actual_sum - nums_sum;
+    }
+
+}
