8ede6d85d474f11be225152756929952c7c66693
Fahmida Yesmin Abedah <fahmida241-35-396@diu.edu.bd>
2026-09-18 11:13:27 +0600

    Solve

---
+++ src/_49_GroupAnagrams.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class _49_GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return new ArrayList<>();
+        }
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
+//TC: \(O(N \times K \log K)\) — To loop through N words and sort K characters per word.
+// SC: \(O(N \times K)\) — To store all N words of length K inside the HashMap.
