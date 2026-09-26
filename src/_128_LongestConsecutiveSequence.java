ee484581a159bc5d6ada88d8bd4fe6f07c473c4f
Fahmida Yesmin Abedah <fahmida241-35-396@diu.edu.bd>
2026-09-26 11:23:55 +0600

    Solve

---
+++ src/_128_LongestConsecutiveSequence.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+
+public class _128_LongestConsecutiveSequence {
+    public int longestConsecutive(int[] nums) {
+        HashMap<Integer, Boolean> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, true);
+        }
+        int longest = 0;
+        for (int num : map.keySet()) {
+            if (!map.containsKey(num - 1)) {
+                int current = num;
+                int count = 1;
+                while (map.containsKey(current + 1)) {
+                    current++;
+                    count++;
+                }
+                longest = Math.max(longest, count);
+            }
+        }
+        return longest;
+    }
+}
+//TC = O(n)
+//SC = O(n)
